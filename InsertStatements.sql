981f6ca5a326f29f9ecc086a3bc18659481bbdb2
acernst <aernst1586@gmail.com>
2015-05-30 09:22:43 -0400

    Updated_5_30

---
+++ InsertStatements.sql
@@ -0,0 +1,9 @@
+-- G = 1 PG = 2 PG13= 3 R= 4, NC17 =5
+-- 1 = Action, 2= Drama, 3= Comadey
+-- 1 = WarnerBros 2= Paramount
+
+Insert into dbo.Movie values('Batman Returns', 1993, 3, 1, 1, 1);
+
+Insert into dbo.Movie values('Rocky', 1989, 3, 1, 2, 2);
+
+Insert into dbo.Movie values('American Beauty', 2003, 4, 2, 2, 3);
